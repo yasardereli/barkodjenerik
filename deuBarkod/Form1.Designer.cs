d7619106793e37b009aa1992e947f29510ee9fe8
Yasar Dereli <yasar.dereli@gmail.com>
2018-11-01 10:08:07 +0300

    Jenerik barkod yazdırma uygulaması
    Uygulama paramterik olarak çalışmaktadır.
    Uygulama exe dosyasından sonra
    1. parametre barkod basım adedi
    2. parametre ZPL barkod kodunu içeren text dosya adı.

---
+++ deuBarkod/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace deuBarkod
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbYaziciAdi = new System.Windows.Forms.TextBox();
+            this.tbAdet = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnYazdir = new System.Windows.Forms.Button();
+            this.tbYazilacak = new System.Windows.Forms.TextBox();
+            this.tbYazilacakDosya = new System.Windows.Forms.TextBox();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.tbYazilacakDosya);
+            this.groupBox2.Controls.Add(this.tbYazilacak);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.tbYaziciAdi);
+            this.groupBox2.Controls.Add(this.tbAdet);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.btnYazdir);
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(442, 130);
+            this.groupBox2.TabIndex = 13;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Etiketi Bas";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(123, 18);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 47;
+            this.label4.Text = "Yazıcı Adı";
+            // 
+            // tbYaziciAdi
+            // 
+            this.tbYaziciAdi.Location = new System.Drawing.Point(126, 34);
+            this.tbYaziciAdi.Name = "tbYaziciAdi";
+            this.tbYaziciAdi.Size = new System.Drawing.Size(138, 20);
+            this.tbYaziciAdi.TabIndex = 46;
+            this.tbYaziciAdi.Text = "Zebra";
+            // 
+            // tbAdet
+            // 
+            this.tbAdet.FormattingEnabled = true;
+            this.tbAdet.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "10",
+            "20",
+            "30",
+            "40",
+            "50",
+            "60",
+            "70",
+            "80",
+            "90",
+            "100"});
+            this.tbAdet.Location = new System.Drawing.Point(21, 34);
+            this.tbAdet.Name = "tbAdet";
+            this.tbAdet.Size = new System.Drawing.Size(80, 21);
+            this.tbAdet.TabIndex = 41;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(25, 17);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 13);
+            this.label5.TabIndex = 37;
+            this.label5.Text = "Adet";
+            // 
+            // btnYazdir
+            // 
+            this.btnYazdir.Location = new System.Drawing.Point(299, 24);
+            this.btnYazdir.Name = "btnYazdir";
+            this.btnYazdir.Size = new System.Drawing.Size(106, 38);
+            this.btnYazdir.TabIndex = 11;
+            this.btnYazdir.Text = "SF ";
+            this.btnYazdir.UseVisualStyleBackColor = true;
+            this.btnYazdir.Click += new System.EventHandler(this.btnYazdir_Click);
+            // 
+            // tbYazilacak
+            // 
+            this.tbYazilacak.Location = new System.Drawing.Point(21, 77);
+            this.tbYazilacak.Multiline = true;
+            this.tbYazilacak.Name = "tbYazilacak";
+            this.tbYazilacak.Size = new System.Drawing.Size(171, 20);
+            this.tbYazilacak.TabIndex = 23;
+            // 
+            // tbYazilacakDosya
+            // 
+            this.tbYazilacakDosya.Location = new System.Drawing.Point(215, 77);
+            this.tbYazilacakDosya.Name = "tbYazilacakDosya";
+            this.tbYazilacakDosya.Size = new System.Drawing.Size(171, 20);
+            this.tbYazilacakDosya.TabIndex = 48;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(472, 150);
+            this.Controls.Add(this.groupBox2);
+            this.Name = "Form1";
+            this.Text = "Etiket Yazdırma - DEU Bilgi İşlem";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnYazdir;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox tbAdet;
+        private System.Windows.Forms.TextBox tbYazilacak;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbYaziciAdi;
+        private System.Windows.Forms.TextBox tbYazilacakDosya;
+    }
+}
+
---
+++ deuBarkod/Form1.cs
@@ -0,0 +1,321 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.IO;
+using System.Diagnostics;
+
+
+
+using System.Drawing.Printing;
+using System.Runtime.InteropServices;
+using deuBarkod.EczaneIlacWS;
+
+namespace deuBarkod
+{
+    public partial class Form1 : Form
+    {
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("kernel32")]
+        private static extern int GetPrivateProfileString(string sekme, string anahtar, string def, StringBuilder retVal, int size, string inidosya); //ini dosyasından okuma apisi
+
+        public string iniOku(String sekme, String anahtar, String iniDosya)
+        {
+            StringBuilder anahtarDeger = new StringBuilder(255);
+            GetPrivateProfileString(sekme, anahtar, "", anahtarDeger, 255, iniDosya);
+            return anahtarDeger.ToString();
+        }
+
+        public String dosyaOku(String filePath)
+        {
+            String barkodData="";
+
+            barkodData = System.IO.File.ReadAllText(@filePath);
+
+
+            return barkodData;
+        }
+
+
+        private String replaceTurString(String turString)
+        {
+            String x = "";
+
+            x = turString.Replace("İ", "I");
+            x = x.Replace("Ş", "S");
+            x = x.Replace("Ü", "U");
+            x = x.Replace("Ğ", "G");
+//            x = x.Replace("Ç", "C");
+
+            return x;
+        }
+
+        private String GetBarkodData(string param1, string param2)
+        {
+            String etiketGenislik;
+            String etiketYukseklik;
+            try
+            {
+                 etiketGenislik = iniOku("AYAR", "etiketGenislik", ".\\config.ini");
+                 etiketYukseklik = iniOku("AYAR", "etiketYukseklik", ".\\config.ini");
+            }
+            catch (Exception ex)
+            {                
+                etiketGenislik = "q400";
+                etiketYukseklik = "Q204,23";            
+            }
+
+            String solBosluk = "90";
+            String barkod = Environment.NewLine + "N" + Environment.NewLine +
+                etiketGenislik + Environment.NewLine +
+                etiketYukseklik + Environment.NewLine +
+                /*
+//               "B0" + solBosluk + ",10,0,3,2,5,50,N,\"" + tbBarkod.Text + "\"" + Environment.NewLine +
+               "A0" + solBosluk + ",40,0,4,1,2,N,\"" + param1 + "\"" + Environment.NewLine +
+               "A0" + solBosluk + ",100,0,4,1,2,N,\"" + param2 + "\"" + Environment.NewLine +
+               "A0" + solBosluk + ",150,0,4,1,2,N,\"" + tbTarih.Text + "\"" + Environment.NewLine +
+            /*  "A0" + solBosluk + ",150,0,2,1,1,N,\"" + "ILAC ADI, DOZU, FORMU:" + "\"" + Environment.NewLine +
+               "A0" + solBosluk + ",180,0,3,1,1,N,\"" + replaceTurString(tb1.Text) + "\"" + Environment.NewLine +
+               "A0" + solBosluk + ",230,0,2,1,1,N,\"" + "SON KULLANMA TARIHI:" + "\"" + Environment.NewLine +
+               "A0" + solBosluk + ",260,0,3,1,1,N,\"" + replaceTurString(tbTarih.Text) + "\"" + Environment.NewLine +
+               "A0" + solBosluk + ",350,0,3,1,1,N,\"" + "KULLANILAN ILAC/BESINLERE" + "\"" + Environment.NewLine +
+               "A0" + solBosluk + ",380,0,3,1,1,N,\"" + "ETKILESIMI RxMediaPharma'DAN" + "\"" + Environment.NewLine +
+               "A0" + solBosluk + ",410,0,3,1,1,N,\"" + "KONTROL EDILMELIDIR." + "\"" + Environment.NewLine +        */       
+              "P1" + Environment.NewLine;
+
+            return barkod;
+        }
+
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           // tbTarih.Text = DateTime.Now.ToShortDateString();
+            tbYaziciAdi.Text = iniOku("AYAR", "tbYaziciAdi", ".\\config.ini");
+            String[] arguments = Environment.GetCommandLineArgs();
+
+           // tbYazilacak.Text = "GetCommandLineArgs: " + String.Join(", ", arguments);
+            try
+            {
+                int baskiAdet = 1;
+
+               
+                try
+                {
+                    baskiAdet = Convert.ToInt32(arguments[1].ToString());
+                }
+                catch { }
+
+                tbYazilacakDosya.Text = arguments[2].ToString();
+
+                String barkodData=dosyaOku(tbYazilacakDosya.Text);
+                yazdirCmdLine(baskiAdet, barkodData);
+
+                if (arguments[2] != null)
+                {
+                    Application.Exit();
+                }
+              
+
+            }
+            catch { }
+
+        }
+
+
+        private void yazdirCmdLine(int baskiAdet=1, String barkodData="")
+        {
+
+            tbYazilacak.Clear();
+
+            try
+            {
+             
+                for (int i = 0; i < baskiAdet; i++)
+                {
+                    tbYazilacak.AppendText(barkodData);
+                }
+
+                RawPrinterHelper.SendStringToPrinter(tbYaziciAdi.Text, tbYazilacak.Text);
+
+            }
+            catch
+            {
+
+            }
+        }
+
+
+        private void yazdir(string param1, string param2)
+        {
+           
+            tbYazilacak.Clear();
+
+            try
+            {
+                int baskiAdet = 1;
+                try
+                {
+                    baskiAdet = Convert.ToInt32(tbAdet.Text);
+                }
+                catch (Exception ex)
+                {
+                    baskiAdet = 1;
+                }
+
+                for (int i = 0; i < baskiAdet; i++)
+                {
+                    tbYazilacak.AppendText(GetBarkodData(param1, param2));
+                }
+
+                RawPrinterHelper.SendStringToPrinter(tbYaziciAdi.Text, tbYazilacak.Text);
+
+            }
+            catch
+            {
+
+            }        
+        }
+
+        private void btnYazdir_Click(object sender, EventArgs e)
+        {
+            yazdir("SF AMP.","");
+        }
+
+     
+
+ 
+
+ 
+ 
+    }
+
+
+
+ public class RawPrinterHelper
+    {
+
+
+        // Structure and API declarions:
+        [StructLayout(LayoutKind.Sequential, CharSet=CharSet.Ansi)]
+        public class DOCINFOA
+        {
+            [MarshalAs(UnmanagedType.LPStr)] public string pDocName;
+            [MarshalAs(UnmanagedType.LPStr)] public string pOutputFile;
+            [MarshalAs(UnmanagedType.LPStr)] public string pDataType;
+        }
+        [DllImport("winspool.Drv", EntryPoint="OpenPrinterA", SetLastError=true, CharSet=CharSet.Ansi, ExactSpelling=true, CallingConvention=CallingConvention.StdCall)]
+        public static extern bool OpenPrinter([MarshalAs(UnmanagedType.LPStr)] string szPrinter, out IntPtr hPrinter, IntPtr pd);
+
+        [DllImport("winspool.Drv", EntryPoint="ClosePrinter", SetLastError=true, ExactSpelling=true, CallingConvention=CallingConvention.StdCall)]
+        public static extern bool ClosePrinter(IntPtr hPrinter);
+
+        [DllImport("winspool.Drv", EntryPoint="StartDocPrinterA", SetLastError=true, CharSet=CharSet.Ansi, ExactSpelling=true, CallingConvention=CallingConvention.StdCall)]
+        public static extern bool StartDocPrinter( IntPtr hPrinter, Int32 level,  [In, MarshalAs(UnmanagedType.LPStruct)] DOCINFOA di);
+
+        [DllImport("winspool.Drv", EntryPoint="EndDocPrinter", SetLastError=true, ExactSpelling=true, CallingConvention=CallingConvention.StdCall)]
+        public static extern bool EndDocPrinter(IntPtr hPrinter);
+
+        [DllImport("winspool.Drv", EntryPoint="StartPagePrinter", SetLastError=true, ExactSpelling=true, CallingConvention=CallingConvention.StdCall)]
+        public static extern bool StartPagePrinter(IntPtr hPrinter);
+
+        [DllImport("winspool.Drv", EntryPoint="EndPagePrinter", SetLastError=true, ExactSpelling=true, CallingConvention=CallingConvention.StdCall)]
+        public static extern bool EndPagePrinter(IntPtr hPrinter);
+
+        [DllImport("winspool.Drv", EntryPoint="WritePrinter", SetLastError=true, ExactSpelling=true, CallingConvention=CallingConvention.StdCall)]
+        public static extern bool WritePrinter(IntPtr hPrinter, IntPtr pBytes, Int32 dwCount, out Int32 dwWritten );
+
+        // SendBytesToPrinter()
+        // When the function is given a printer name and an unmanaged array
+        // of bytes, the function sends those bytes to the print queue.
+        // Returns true on success, false on failure.
+        public static bool SendBytesToPrinter( string szPrinterName, IntPtr pBytes, Int32 dwCount)
+        {
+            Int32    dwError = 0, dwWritten = 0;
+            IntPtr    hPrinter = new IntPtr(0);
+            DOCINFOA    di = new DOCINFOA();
+            bool    bSuccess = false; // Assume failure unless you specifically succeed.
+
+            di.pDocName = "My C#.NET RAW Document";
+            di.pDataType = "RAW";
+
+            // Open the printer.
+            if( OpenPrinter( szPrinterName.Normalize(), out hPrinter, IntPtr.Zero ) )
+            {
+                // Start a document.
+                if( StartDocPrinter(hPrinter, 1, di) )
+                {
+                    // Start a page.
+                    if( StartPagePrinter(hPrinter) )
+                    {
+                        // Write your bytes.
+                        bSuccess = WritePrinter(hPrinter, pBytes, dwCount, out dwWritten);
+                        EndPagePrinter(hPrinter);
+                    }
+                    EndDocPrinter(hPrinter);
+                }
+                ClosePrinter(hPrinter);
+            }
+            // If you did not succeed, GetLastError may give more information
+            // about why not.
+            if( bSuccess == false )
+            {
+                    dwError = Marshal.GetLastWin32Error();
+            }
+            return bSuccess;
+        }
+
+        public static bool SendFileToPrinter( string szPrinterName, string szFileName )
+        {
+            // Open the file.
+            FileStream fs = new FileStream(szFileName, FileMode.Open);
+            // Create a BinaryReader on the file.
+            BinaryReader br = new BinaryReader(fs);
+            // Dim an array of bytes big enough to hold the file's contents.
+            Byte []bytes = new Byte[fs.Length];
+            bool bSuccess = false;
+            // Your unmanaged pointer.
+            IntPtr pUnmanagedBytes = new IntPtr(0);
+            int nLength;
+
+            nLength = Convert.ToInt32(fs.Length);
+            // Read the contents of the file into the array.
+            bytes = br.ReadBytes( nLength );
+            // Allocate some unmanaged memory for those bytes.
+            pUnmanagedBytes = Marshal.AllocCoTaskMem(nLength);
+            // Copy the managed byte array into the unmanaged array.
+            Marshal.Copy(bytes, 0, pUnmanagedBytes, nLength);
+            // Send the unmanaged bytes to the printer.
+            bSuccess = SendBytesToPrinter(szPrinterName, pUnmanagedBytes, nLength);
+            // Free the unmanaged memory that you allocated earlier.
+            Marshal.FreeCoTaskMem(pUnmanagedBytes);
+            return bSuccess;
+        }
+        public static bool SendStringToPrinter( string szPrinterName, string szString )
+        {
+            IntPtr pBytes;
+            Int32 dwCount;
+            // How many characters are in the string?
+            dwCount = szString.Length;
+            // Assume that the printer is expecting ANSI text, and then convert
+            // the string to ANSI text.
+            pBytes = Marshal.StringToCoTaskMemAnsi(szString);
+            // Send the converted ANSI string to the printer.
+            SendBytesToPrinter(szPrinterName, pBytes, dwCount);
+            Marshal.FreeCoTaskMem(pBytes);
+            return true;
+        }
+    }
+}
